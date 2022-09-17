e2b498fd78bbee135eb0fe776c0d31c170ae1e82
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-17 15:31:05 +0200

    initial navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+const pages = ['about', 'work', 'contact'];
+
+const ResponsiveAppBar = () => {
   return (
-    <div>
-    
-    </div>
-  )
-}
+    <AppBar position="fixed" style={{background: 'transparent'}}>
+      <Container maxWidth="7xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', justifyContent: 'flex-end' } }}>
+              <Button 
+                sx={{ my: 2, color: 'black', display: 'block', '&:hover': {background: '#FFD9DA'}, margin: 2}}
+              >
+                <Link style={{textDecoration: 'none', color: 'black'}} to={`/`}>HOME</Link>
+              </Button>
+            {pages.map((page) => (
+              <Button 
+                key={page}
+                sx={{ my: 2, color: 'black', display: 'block', '&:hover': {background: '#FFD9DA'}, margin: 2}}
+              >
+                <Link style={{textDecoration: 'none', color: 'black'}} to={`/${page}`}>{page}</Link>
+              </Button>
+            ))}
+            </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default ResponsiveAppBar;
