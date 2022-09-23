fcd88861b12aaab08ac12365adbc522280b77ba8
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-23 15:58:32 +0200

    adjusted framer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,36 +8,31 @@ import './Home.css';
 function Home() {
   return (
     <div className='Home'>
-      <div>
         {/* SMALL SCREENS */}
         <Box sx={{display: {xs: 'flex', md: 'none'}}}>
 
         </Box>
+
         {/* NORMAL - LARGE SCREENS */}
         <Box sx={{display: {xs: 'none', md: 'flex'}}} className='home-first-section-large' >
-        <div style={{marginLeft: '5vw'}}> 
-          <motion.div 
-          whileHover={{scale: 1.2}}
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          >
-            <h1 style={{fontSize: '8vw', color: 'white', margin: 0}}>Betti Angyan</h1>
-          </motion.div>
-          <motion.div 
-          whileHover={{scale: 1.2}}
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          >
-            <h2 style={{fontSize: '3vw', color: 'white', marginLeft: '5vw', paddingBottom: '5vw'}}>Makeup Artist.</h2>
-          </motion.div>
-        </div>
+          <div style={{marginLeft: '5vw'}}> 
+            <motion.div 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{ ease: "easeOut", duration: 2 }}
+            >
+              <h1 style={{fontSize: '8vw', color: 'white', margin: 0}}>Betti Angyan</h1>
+              <h2 style={{fontSize: '3vw', color: 'white', marginLeft: '5vw', paddingBottom: '5vw'}}>Makeup Artist.</h2>
+            </motion.div>
+          </div>
           <div>
-            <IconButton>
-              <ArrowDownwardIcon/>
-            </IconButton>
+            <motion.div whileHover={{scale: 1.3}}>
+              <IconButton>
+                  <ArrowDownwardIcon/>
+                </IconButton>
+              </motion.div>
           </div>
         </Box>
-      </div>
     </div>
   )
 }
