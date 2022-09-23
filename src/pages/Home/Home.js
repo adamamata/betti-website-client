c7c11e32f829256e29c7c4ae79c85be0d636fe32
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-23 12:55:12 +0200

    added photos to assets

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,9 +4,6 @@ import './Home.css';
 function Home() {
   return (
     <div className='Home'>
-      <div className='home-first-section'>
-        {/* Title here */}
-      </div>
     </div>
   )
 }
