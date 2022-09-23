54478c0fdbec2edba92c6a910ceff1702ca9e35b
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-23 12:31:51 +0200

    added styling to home first section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,9 @@ import './Home.css';
 function Home() {
   return (
     <div className='Home'>
+      <div className='home-first-section'>
+        {/* Title here */}
+      </div>
     </div>
   )
 }
