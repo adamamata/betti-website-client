8e8945a12ec40cc8586202eda32a94374c3996d7
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-09-25 16:45:01 +0200

    added scroll functionality up

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { IconButton } from '@mui/material';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import { motion } from 'framer-motion';
 import { useRef } from 'react';
 import './Home.css';
 
 function Home() {
   const work = useRef(null);
+  const top = useRef(null);
 
   const scroll = (e) => {
     window.scrollTo({
@@ -25,15 +27,16 @@ function Home() {
 
         {/* NORMAL - LARGE SCREENS */}
         {/* TOP SECTION */}
-        <Box sx={{display: {xs: 'none', md: 'flex'}}} className='home-top-large'>
-          <div className='top-section' style={{marginLeft: '5vw'}}> 
+        <Box sx={{display: {xs: 'none', md: 'flex'}}} className='home-top-large' ref={top}>
+          <div className='top-contents'> 
             <motion.div 
+            style={{marginLeft: '5vw'}}
             initial={{opacity: 0}}
             animate={{opacity: 1}}
             transition={{ ease: "easeOut", duration: 2 }}
             >
               <h1 style={{fontSize: '8vw', color: 'white', margin: 0}}>Betti Angyan</h1>
-              <h2 style={{fontSize: '3vw', color: 'white', marginLeft: '5vw', paddingBottom: '5vw'}}>Makeup Artist.</h2>
+              <h2 style={{fontSize: '3vw', color: 'white', marginLeft: '5vw', paddingBottom: '5vw'}}>Helping client's realize how beautiful they really are.</h2>
             </motion.div>
           </div>
           <div>
@@ -45,7 +48,13 @@ function Home() {
         
         {/* WORK SECTION */}
         <Box sx={{display: {xs: 'none', md: 'flex'}}} className='home-work-large' ref={work}>
+          <div className='work-contents'>
+            <h1 style={{fontSize: '8vw', color: 'white', marginRight: '5vw'}}>My Portfolio</h1>
+          </div>
           <div>
+            <IconButton sx={{'&:hover': {background: 'white'}, marginBottom: 1}} onClick={() => scroll(top)}>
+              <ArrowUpwardIcon/>
+            </IconButton>
           </div>
         </Box>
     </div>
