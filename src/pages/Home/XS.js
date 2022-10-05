52fe77ee14f20e34789dca4938727586abbbc490
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-05 21:55:23 +0200

    home page done

--- src/pages/Home/XS.js
+++ src/pages/Home/XS.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
 import Box from '@mui/material/Box';
 import { motion } from 'framer-motion';
+import Slider from '../../components/Slider';
 
 function XS() {
   return (
@@ -13,10 +14,19 @@ function XS() {
             animate={{opacity: 1}}
             transition={{ ease: "easeOut", duration: 2 }}
             >
-              <h1 style={{fontSize: '20vw', color: 'white', margin: 0}}>Betti Angyan</h1>
+              <h1 style={{fontSize: '10vw', color: 'white', margin: 0}}>Betti Angyan</h1>
               <h2 style={{fontSize: '7vw', color: 'white', paddingBottom: '5vw'}}>Helping clients realize how beautiful they really are.</h2>
             </motion.div>
           </div>
+        </Box>
+        <Box sx={{display: {xs: 'flex', md: 'none'}}} className='home-work-large'>
+          <div className='work-contents'>
+            <h1 style={{fontSize: '10vw', color: 'white', marginRight: '5vw'}}>My Portfolio</h1>
+          </div>
+          {/* SLIDER */}
+            <p style={{color: 'white'}}>swipe to see more.</p>
+            <Slider /> 
+          {/* ICON BUTTON */}
         </Box>
     </div>
   )
