75587fd94494e2f1094837c40dfd1267f4f046ae
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-19 19:07:05 +0200

    initial form setup

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Contact.css';
+import { TextField, Button } from '@mui/material'
 
 function Contact() {
   return (
     <div className='Contact'>
-      <div>
-        
-      </div>
+      <form>
+        <TextField 
+          className='text-field'
+          type='text'
+          label='First Name'
+          variant='outlined'
+        />
+        <TextField
+          className='text-field'
+          type='text'
+          label='Last Name'
+          variant='outlined'
+        />
+        <TextField  
+          className='text-field'
+          type='text'
+          label='Message'
+          variant='outlined'
+        />
+        <Button>Submit</Button>
+      </form>
     </div>
   )
 }
