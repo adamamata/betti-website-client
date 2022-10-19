a41bdb9e17e87d753dcb46156fe9d9cb68c025e5
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-19 19:20:55 +0200

    changing it to be one page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Contact from './pages/Contact/Contact';
+// import About from './pages/About/About';
+// import Contact from './pages/Contact/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path ='/contact' element={<Contact/>}/>
+        {/* <Route path='/about' element={<About/>}/>
+        <Route path ='/contact' element={<Contact/>}/> */}
       </Routes>
     </div>
   );
