62608fad461e67ec93974a3772ee14e1f1991725
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-19 19:34:49 +0200

    one page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 // import About from './pages/About/About';
 // import Contact from './pages/Contact/Contact';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      {/* <Navbar/> */}
       <Routes>
         <Route path='/' element={<Home/>}/>
         {/* <Route path='/about' element={<About/>}/>
