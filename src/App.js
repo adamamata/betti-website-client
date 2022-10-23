600f722836daa48628455948ee974dda72fafc55
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-10-23 19:14:58 +0200

    no navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-// import About from './pages/About/About';
-// import Contact from './pages/Contact/Contact';
-// import Navbar from './components/Navbar';
-
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
       <Routes>
         <Route path='/' element={<Home/>}/>
-        {/* <Route path='/about' element={<About/>}/>
-        <Route path ='/contact' element={<Contact/>}/> */}
       </Routes>
     </div>
   );
